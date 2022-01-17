7af76970259a9f536e238497349e0a7005718ffa
Vancuum <89651416+Vancuum@users.noreply.github.com>
2022-01-16 20:16:28 -0800

    Additional Functionality, partially working

--- lab1_base.asm
+++ lab1_base.asm
@@ -16,6 +16,8 @@ LCD_D4 equ P3.4
 LCD_D5 equ P3.5
 LCD_D6 equ P3.6
 LCD_D7 equ P3.7
+LCD_LEFT equ p2.0 ;For scrolling the LCD to the left
+LCD_RIGHT equ p2.3 ; For scrolling the LCD to the right
 
 ; When using a 22.1184MHz crystal in fast mode
 ; one cycle takes 1.0/22.1184MHz = 45.21123 ns
@@ -47,7 +49,7 @@ L1: djnz R0, L1 ; 3 cycles->3*45.21123ns*166=22.51519us
     pop AR1
     pop AR0
     ret
-
+    
 ;---------------------------------;
 ; Toggles the LCD's 'E' pin       ;
 ;---------------------------------;
@@ -121,7 +123,7 @@ LCD_4BIT:
     lcall WriteCommand
 
     ; Configure the LCD
-    mov a, #0x28
+    mov a, #0x20
     lcall WriteCommand
     mov a, #0x0c
     lcall WriteCommand
@@ -139,14 +141,60 @@ LCD_4BIT:
 
 ;Writes the string pointed to by dptr(must be null-terminated)
 WriteString:
+	push AR0
+	mov R1, #79 ;LCD holds 80 characters
+nextChar:
 	clr a
 	movc a, @a+dptr
 	jz str_end
 	lcall WriteData
 	inc dptr
-	sjmp WriteString
-str_end: ret
+	djnz R1, nextChar
+	lcall CheckForScroll
+	sjmp nextChar
+str_end: 
+	pop AR1
+	ret
+
+;LCD can only hold 80 characters, so need
+;to keep track of how many characters have been read
+;and add new characters once they are requested(through scrolling).
+CheckForScroll:
+	jnb LCD_LEFT, call_left
+	jnb LCD_RIGHT, call_right
+	sjmp CheckForScroll
+;Need to check if we have reached these subroutines from the forever loop
+;or from the checkForScroll(i.e., do more characters need to be written?)
+;R0 will be 0 if we have not reached the forever loop yet
+call_left:
+	inc R1
+	lcall Scroll_left
+    cjne R0, #0, forever
+    sjmp nextChar
+call_right:
+	lcall Scroll_right
+	cjne R0, #0, forever
+	sjmp CheckForScroll
 	
+Scroll_left:
+	mov a, #0x18 ;Instruction code for shifting the display to the left
+	lcall WriteCommand
+	;Wait 1 second
+	push AR2	
+	mov R2, #100
+	lcall WaitMilliSec
+	pop AR2
+	ret
+	
+Scroll_right:
+	mov a, #0x1C ;Instruction code for shifting to the right
+	lcall WriteCommand
+	;Wait 1 second
+	push AR2
+	mov R2, #100
+	lcall WaitMilliSec
+	pop AR2
+	ret
 ;---------------------------------;
 ; Main loop.  Initialize stack,   ;
 ; ports, LCD, and displays        ;
@@ -154,29 +202,34 @@ str_end: ret
 ;---------------------------------;
 myprogram:
     mov SP, #7FH
+    push AR0
+    MOV AR0, #0
     lcall LCD_4BIT
     mov a, #0x80 ; Move cursor to line 1 column 1
     lcall WriteCommand
-	mov dptr, #number
+	;mov dptr, #number
+	;lcall WriteString
+	;mov dptr, #name
+	;lcall WriteString
+	mov dptr, #tale_of_two_cities
 	lcall WriteString
-	mov dptr, #first_name
-	lcall WriteString
-	mov a, #0xC2
-	lcall WriteCommand
-	mov dptr, #last_name
-	lcall WriteString
+	mov R0, #1
 	
 forever:
-    sjmp forever
+	jnb LCD_LEFT, call_left
+	jnb LCD_RIGHT, call_right
+	sjmp forever
 	
 ;String which stores my name
-first_name:
-	db ' Bennett', 0
-	
-last_name:
-	db ' Galamaga', 0
+name:
+	db ' Bennett Galamaga', 0
 
 ;String which stores my student number
 number:
 	db '84372515', 0
+	
+tale_of_two_cities:
+	db 'A TALE OF TWO CITIES  A STORY OF THE FRENCH REVOLUTION  By Charles Dickens CHAPTER I. The Period '
+	db 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of '
+	db 'foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way—in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only.There were a king with a large jaw and a queen with a plain face, on the throne of England; there were a king with a large jaw and a queen with a fair face, on the throne of France. In both countries it was clearer than crystal to the lords of the State preserves of loaves and fishes, that things in general were settled for ever.It was the year of Our Lord one thousand seven hundred and seventy-five. Spiritual revelations were conceded to England at that favoured period, as at this. Mrs. Southcott had recently attained her five-and-twentieth blessed birthday, of whom a prophetic private in the Life Guards had heralded the sublime appearance by announcing that arrangements were made for the swallowing up of London and Westminster. Even the Cock-lane ghost had been laid only a round dozen of years, after rapping out its messages, as the spirits of this very year last past (supernaturally deficient in originality) rapped out theirs. Mere messages in the earthly order of events had lately come to the English Crown and People, from a congress of British subjects in America: which, strange to relate, have proved more important to the human race than any communications yet received through any of the chickens of the Cock-lane brood.France, less favoured on the whole as to matters spiritual than her sister of the shield and trident, rolled with exceeding smoothness down hill, making paper money and spending it. Under the guidance of her Christian pastors, she entertained herself, besides, with such humane achievements as sentencing a youth to have his hands cut off, his tongue torn out with pincers, and his body burned alive, because he had not kneeled down in the rain to do honour to a dirty procession of monks which passed within his view, at a distance of some fifty or sixty yards. It is likely enough that, rooted in the woods of France and Norway, there were growing trees, when that sufferer was put to death, already marked by the Woodman, Fate, to come down and be sawn into boards, to make a certain movable framework with a sack and a knife in it, terrible in history. It is likely enough that in the rough outhouses of some tillers of the heavy lands adjacent to Paris, there were sheltered from the weather that very day, rude carts, bespattered with rustic mire, snuffed about by pigs, and roosted in by poultry, which the Farmer, Death, had already set apart to be his tumbrils of the Revolution. But that Woodman and that Farmer, though they work unceasingly, work silently, and no one heard them as they went about with muffled tread: the rather, forasmuch as to entertain any suspicion that they were awake, was to be atheistical and traitorous. ,In England, there was scarcely an amount of order and protection to justify much national boasting. Daring burglaries by armed men, and highway robberies, took place in the capital itself every night; families were publicly cautioned not to go out of town without removing their furniture to upholsterers’ warehouses for security; the highwayman in the dark was a City tradesman in the light, and, being recognised and challenged by his fellow-tradesman whom he stopped in his character of “the Captain,” gallantly shot him through the head and rode away; the mail was waylaid by seven robbers, and the guard shot three dead, and then got shot dead himself by the other four, “in consequence of the failure of his ammunition:” after which the mail was robbed in peace; that magnificent potentate, the Lord Mayor of London, was made to stand and deliver on Turnham Green, by one highwayman, who despoiled the illustrious creature in sight of all his retinue; prisoners in London gaols fought battles with their turnkeys, and the majesty of the law fired blunderbusses in among them, loaded with rounds of shot and ball; thieves snipped off diamond crosses from the necks of noble lords at Court drawing-rooms; musketeers went into St. Giles’s, to search for contraband goods, and the mob fired on the musketeers, and the musketeers fired on the mob, and nobody thought any of these occurrences much out of the common way. In the midst of them, the hangman, ever busy and ever worse than useless, was in constant requisition; now, stringing up long rows of miscellaneous criminals; now, hanging a housebreaker on Saturday who had been taken on Tuesday; now, burning people in the hand at Newgate by the dozen, and now burning pamphlets at the door of Westminster Hall; to-day, taking the life of an atrocious murderer, and to-morrow of a wretched pilferer who had robbed a farmer’s boy of sixpence. All these things, and a thousand like them, came to pass in and close upon the dear old year one thousand seven hundred and seventy-five. Environed by them, while the Woodman and the Farmer worked unheeded, those two of the large jaws, and those other two of the plain and the fair faces, trod with stir enough, and carried their divine rights with a high hand. Thus did the year one thousand seven hundred and seventy-five conduct their Greatnesses, and myriads of small creatures—the creatures of this chronicle among the rest—along the roads that lay before them.', 0
 END
